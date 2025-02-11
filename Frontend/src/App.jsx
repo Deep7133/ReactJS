09ebe04254486d5d0448e30d91e77231c9b5bc8d
Deep7133 <deepmenpara9@gmail.com>
2025-02-11 11:51:47 +0530

    added

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from '../components/Footer.jsx'
+import Greet from '../components/Greet.jsx'
+import Header from '../components/Header.jsx'
+import MainComponent from '../components/MainComponent.jsx'
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Header />
+     <MainComponent />
+     <Footer />
+     </>
+  )
+}
+
+export default App
